6fff384a61a7d5bf09613cf495d648a65add5f4a
Serge Vakulenko <serge.vakulenko@gmail.com>
2024-08-05 23:34:06 -0700

    Enable maxreal in tests.

--- tests/function_test.cpp
+++ tests/function_test.cpp
@@ -8,8 +8,8 @@ TEST_F(x1_machine, function_abs)
         _b_e_g_i_n
             print(abs(-123.456));
             print(abs(123.456));
-_c            print(abs(-1.615850303564⏨616));
-_c            print(abs(1.615850303564⏨616));
+            print(abs(-1.615850303564⏨616));
+            print(abs(1.615850303564⏨616));
             print(abs(-0.0));
             print(abs(0.0));
             print(abs(-67108863));
@@ -20,6 +20,8 @@ _c            print(abs(1.615850303564⏨616));
     )");
     const std::string expect = R"(123.456
 123.456
+1.615850303564e+616
+1.615850303564e+616
 0
 0
 67108863
@@ -36,8 +38,8 @@ TEST_F(x1_machine, function_sign)
         _b_e_g_i_n
             print(sign(-123.456));
             print(sign(123.456));
-_c            print(sign(-1.615850303564⏨616));
-_c            print(sign(1.615850303564⏨616));
+            print(sign(-1.615850303564⏨616));
+            print(sign(1.615850303564⏨616));
             print(sign(-0.0));
             print(sign(0.0));
             print(sign(-1));
@@ -49,6 +51,8 @@ _c            print(sign(1.615850303564⏨616));
         _e_n_d
     )");
     const std::string expect = R"(-1
+1
+-1
 1
 0
 0
