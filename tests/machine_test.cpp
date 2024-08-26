7310d25253beffc94c0d799e9b357f149adba902
Serge Vakulenko <serge.vakulenko@gmail.com>
2024-08-25 23:46:28 -0700

    Enhance tests input_encoding, digraph_encoding, string_quotes.

--- tests/machine_test.cpp
+++ tests/machine_test.cpp
@@ -4,15 +4,12 @@
 
 TEST_F(x1_machine, input_encoding)
 {
-    // Technically it's a wrong program, as print() accepts only
-    // a number on input. But for checking encoding it's all right.
-    // We are not going to run it.
-    compile(R"(
+    auto output = compile_and_run(R"(
         b̲e̲g̲i̲n̲
-            print(`0123456789');
-            print(`abcdefghijklmnopqrstuvwxyz');
-            print(`ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-            print(`+-*×/>=<¬∧∨,.⏨@:; ()[]');
+            PRINTTEXT(`0123456789'); NLCR;
+            PRINTTEXT(`abcdefghijklmnopqrstuvwxyz'); NLCR;
+            PRINTTEXT(`ABCDEFGHIJKLMNOPQRSTUVWXYZ'); NLCR;
+            PRINTTEXT(`+-*×/>=<¬∧∨,.⏨@:; ()[]'); NLCR;
         e̲n̲d̲
     )");
     // Note: symbols " ? _ are prohibited in strings.
@@ -21,92 +18,111 @@ TEST_F(x1_machine, input_encoding)
     // Check result in memory.
     unsigned start = machine->get_entry(0);
     EXPECT_EQ(machine->mem_load(start), 96);            // START
-    EXPECT_EQ(machine->mem_load(start+1), 0x02'01'00);  // 0 1 2
-    EXPECT_EQ(machine->mem_load(start+2), 0x05'04'03);  // 3 4 5
-    EXPECT_EQ(machine->mem_load(start+3), 0x08'07'06);  // 6 7 8
-    EXPECT_EQ(machine->mem_load(start+4), 0x00'ff'09);  // 9 \377
-    EXPECT_EQ(machine->mem_load(start+5), 103);         // print
-    EXPECT_EQ(machine->mem_load(start+6), 0x0c'0b'0a);  // a b c
-    EXPECT_EQ(machine->mem_load(start+7), 0x0f'0e'0d);  // d e f
-    EXPECT_EQ(machine->mem_load(start+8), 0x12'11'10);  // g h i
-    EXPECT_EQ(machine->mem_load(start+9), 0x15'14'13);  // j k l
-    EXPECT_EQ(machine->mem_load(start+10), 0x18'17'16); // m n o
-    EXPECT_EQ(machine->mem_load(start+11), 0x1b'1a'19); // p q r
-    EXPECT_EQ(machine->mem_load(start+12), 0x1e'1d'1c); // s t u
-    EXPECT_EQ(machine->mem_load(start+13), 0x21'20'1f); // v w x
-    EXPECT_EQ(machine->mem_load(start+14), 0xff'23'22); // y z \377
-    EXPECT_EQ(machine->mem_load(start+15), 103);        // print
-    EXPECT_EQ(machine->mem_load(start+16), 0x27'26'25); // A B C
-    EXPECT_EQ(machine->mem_load(start+17), 0x2a'29'28); // D E F
-    EXPECT_EQ(machine->mem_load(start+18), 0x2d'2c'2b); // G H I
-    EXPECT_EQ(machine->mem_load(start+19), 0x30'2f'2e); // J K L
-    EXPECT_EQ(machine->mem_load(start+20), 0x33'32'31); // M N O
-    EXPECT_EQ(machine->mem_load(start+21), 0x36'35'34); // P Q R
-    EXPECT_EQ(machine->mem_load(start+22), 0x39'38'37); // S T U
-    EXPECT_EQ(machine->mem_load(start+23), 0x3c'3b'3a); // V W X
-    EXPECT_EQ(machine->mem_load(start+24), 0xff'3e'3d); // Y Z \377
-    EXPECT_EQ(machine->mem_load(start+25), 103);        // print
-    EXPECT_EQ(machine->mem_load(start+26), 0x42'41'40); // + - *
-    EXPECT_EQ(machine->mem_load(start+27), 0x46'43'42); // × / >
-    EXPECT_EQ(machine->mem_load(start+28), 0x4c'4a'48); // = < ¬
-    EXPECT_EQ(machine->mem_load(start+29), 0x57'4e'4d); // ∧ ∨ ,
-    EXPECT_EQ(machine->mem_load(start+30), 0x59'59'58); // . ⏨ @
-    EXPECT_EQ(machine->mem_load(start+31), 0x5d'5b'5a); // : ; space
-    EXPECT_EQ(machine->mem_load(start+32), 0x64'63'62); // ( ) [
-    EXPECT_EQ(machine->mem_load(start+33), 0x00'ff'65); // ] \377
-    EXPECT_EQ(machine->mem_load(start+34), 103);        // print
-    EXPECT_EQ(machine->mem_load(start+35), 97);         // STOP
+    EXPECT_EQ(machine->mem_load(start+3), 0x02'01'00);  // 0 1 2
+    EXPECT_EQ(machine->mem_load(start+4), 0x05'04'03);  // 3 4 5
+    EXPECT_EQ(machine->mem_load(start+5), 0x08'07'06);  // 6 7 8
+    EXPECT_EQ(machine->mem_load(start+6), 0x00'ff'09);  // 9 \377
+
+    EXPECT_EQ(machine->mem_load(start+14), 0x0c'0b'0a);  // a b c
+    EXPECT_EQ(machine->mem_load(start+15), 0x0f'0e'0d);  // d e f
+    EXPECT_EQ(machine->mem_load(start+16), 0x12'11'10);  // g h i
+    EXPECT_EQ(machine->mem_load(start+17), 0x15'14'13);  // j k l
+    EXPECT_EQ(machine->mem_load(start+18), 0x18'17'16); // m n o
+    EXPECT_EQ(machine->mem_load(start+19), 0x1b'1a'19); // p q r
+    EXPECT_EQ(machine->mem_load(start+20), 0x1e'1d'1c); // s t u
+    EXPECT_EQ(machine->mem_load(start+21), 0x21'20'1f); // v w x
+    EXPECT_EQ(machine->mem_load(start+22), 0xff'23'22); // y z \377
+
+    EXPECT_EQ(machine->mem_load(start+30), 0x27'26'25); // A B C
+    EXPECT_EQ(machine->mem_load(start+31), 0x2a'29'28); // D E F
+    EXPECT_EQ(machine->mem_load(start+32), 0x2d'2c'2b); // G H I
+    EXPECT_EQ(machine->mem_load(start+33), 0x30'2f'2e); // J K L
+    EXPECT_EQ(machine->mem_load(start+34), 0x33'32'31); // M N O
+    EXPECT_EQ(machine->mem_load(start+35), 0x36'35'34); // P Q R
+    EXPECT_EQ(machine->mem_load(start+36), 0x39'38'37); // S T U
+    EXPECT_EQ(machine->mem_load(start+37), 0x3c'3b'3a); // V W X
+    EXPECT_EQ(machine->mem_load(start+38), 0xff'3e'3d); // Y Z \377
+
+    EXPECT_EQ(machine->mem_load(start+46), 0x42'41'40); // + - *
+    EXPECT_EQ(machine->mem_load(start+47), 0x46'43'42); // × / >
+    EXPECT_EQ(machine->mem_load(start+48), 0x4c'4a'48); // = < ¬
+    EXPECT_EQ(machine->mem_load(start+49), 0x57'4e'4d); // ∧ ∨ ,
+    EXPECT_EQ(machine->mem_load(start+50), 0x59'59'58); // . ⏨ @
+    EXPECT_EQ(machine->mem_load(start+51), 0x5d'5b'5a); // : ; space
+    EXPECT_EQ(machine->mem_load(start+52), 0x64'63'62); // ( ) [
+    EXPECT_EQ(machine->mem_load(start+53), 0x00'ff'65); // ] \377
+
+    // Check output.
+    const std::string expect = R"(0123456789
+abcdefghijklmnopqrstuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
++-××/>=<¬∧∨,.⏨⏨:; ()[]
+)";
+    EXPECT_EQ(output, expect);
 }
 
 TEST_F(x1_machine, digraph_encoding)
 {
-    compile(R"(
+    auto output = compile_and_run(R"(
         b̲e̲g̲i̲n̲
-            print(` := ');
-            print(` |∧ ');
-            print(` |= ');
-            print(` |<|> ');
-            print(` _> ');
-            print(` _= ');
-            print(` _< ');
-            print(` _¬ ');
-            print(` _: ');
+            PRINTTEXT(``:=''); NLCR;
+            PRINTTEXT(``|∧''); NLCR;
+            PRINTTEXT(``|=''); NLCR;
+            PRINTTEXT(``|<|>''); NLCR;
+            PRINTTEXT(``_>''); NLCR;
+            PRINTTEXT(``_=''); NLCR;
+            PRINTTEXT(``_<''); NLCR;
+            PRINTTEXT(``_¬''); NLCR;
+            PRINTTEXT(``_:''); NLCR;
         e̲n̲d̲
     )");
 
     unsigned start = machine->get_entry(0);
     EXPECT_EQ(machine->mem_load(start), 96);            // START
-    EXPECT_EQ(machine->mem_load(start+1), 0x5d'5c'5d);  // := ≔
-    EXPECT_EQ(machine->mem_load(start+4), 0x5d'45'5d);  // |∧ ↑
-    EXPECT_EQ(machine->mem_load(start+7), 0x5d'4b'5d);  // |= ≠
-    EXPECT_EQ(machine->mem_load(start+10), 0x67'66'5d); // |< |> ` '
-    EXPECT_EQ(machine->mem_load(start+13), 0x5d'47'5d); // _> ≥
-    EXPECT_EQ(machine->mem_load(start+16), 0x5d'50'5d); // _= ≡
-    EXPECT_EQ(machine->mem_load(start+19), 0x5d'49'5d); // _< ≤
-    EXPECT_EQ(machine->mem_load(start+22), 0x5d'4f'5d); // _¬ ⊃
-    EXPECT_EQ(machine->mem_load(start+25), 0x5d'44'5d); // _: ÷
-    EXPECT_EQ(machine->mem_load(start+28), 97);         // STOP
+    EXPECT_EQ(machine->mem_load(start+3), 0x67'5c'66);  // := ≔
+    EXPECT_EQ(machine->mem_load(start+12), 0x67'45'66);  // |∧ ↑
+    EXPECT_EQ(machine->mem_load(start+21), 0x67'4b'66);  // |= ≠
+    EXPECT_EQ(machine->mem_load(start+30), 0x67'66'66); // |< |> ` '
+    EXPECT_EQ(machine->mem_load(start+39), 0x67'47'66); // _> ≥
+    EXPECT_EQ(machine->mem_load(start+48), 0x67'50'66); // _= ≡
+    EXPECT_EQ(machine->mem_load(start+57), 0x67'49'66); // _< ≤
+    EXPECT_EQ(machine->mem_load(start+66), 0x67'4f'66); // _¬ ⊃
+    EXPECT_EQ(machine->mem_load(start+75), 0x67'44'66); // _: ÷
+
+    const std::string expect = R"(`:='
+`↑'
+`≠'
+``''
+`≥'
+`≡'
+`≤'
+`⊃'
+`÷'
+)";
+    EXPECT_EQ(output, expect);
 }
 
 TEST_F(x1_machine, string_quotes)
 {
-    compile(R"(
+    auto output = compile_and_run(R"(
         b̲e̲g̲i̲n̲
-            print(`This is a `string'');
+            PRINTTEXT(`This is a `string'');
+            NLCR
         e̲n̲d̲
     )");
 
     unsigned start = machine->get_entry(0);
     EXPECT_EQ(machine->mem_load(start), 96);           // START
-    EXPECT_EQ(machine->mem_load(start+1), 0x12'11'38); // T h i
-    EXPECT_EQ(machine->mem_load(start+2), 0x12'5d'1c); // s ␣ i
-    EXPECT_EQ(machine->mem_load(start+3), 0x0a'5d'1c); // s ␣ a
-    EXPECT_EQ(machine->mem_load(start+4), 0x1c'66'5d); // ␣ ` s
-    EXPECT_EQ(machine->mem_load(start+5), 0x12'1b'1d); // t r i
-    EXPECT_EQ(machine->mem_load(start+6), 0x67'10'17); // n g '
-    EXPECT_EQ(machine->mem_load(start+7), 0x00'00'ff); // \377
-    EXPECT_EQ(machine->mem_load(start+8), 103);        // print
-    EXPECT_EQ(machine->mem_load(start+9), 97);         // STOP
+    EXPECT_EQ(machine->mem_load(start+3), 0x12'11'38); // T h i
+    EXPECT_EQ(machine->mem_load(start+4), 0x12'5d'1c); // s ␣ i
+    EXPECT_EQ(machine->mem_load(start+5), 0x0a'5d'1c); // s ␣ a
+    EXPECT_EQ(machine->mem_load(start+6), 0x1c'66'5d); // ␣ ` s
+    EXPECT_EQ(machine->mem_load(start+7), 0x12'1b'1d); // t r i
+    EXPECT_EQ(machine->mem_load(start+8), 0x67'10'17); // n g '
+    EXPECT_EQ(machine->mem_load(start+9), 0x00'00'ff); // \377
+
+    const std::string expect = "This is a `string'\n";
+    EXPECT_EQ(output, expect);
 }
 
 TEST_F(x1_machine, virtual_stack)
