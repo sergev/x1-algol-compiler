af9ffd22636df210188a9490c2a858e70fdf77ed
Serge Vakulenko <serge.vakulenko@gmail.com>
2024-09-07 15:47:40 -0700

    Implement input_char() method.

--- simulator/input.cpp
+++ simulator/input.cpp
@@ -144,15 +144,125 @@ unsigned Machine::read_console_switches(std::istream &input_stream, unsigned bit
 
 //
 // Read character from input stream.
-// Return integer number.
+// Return integer number in Algol encoding.
 // On end of file, return 0.
 //
 int Machine::input_char(std::istream &stream)
 {
     char ch;
-    if (stream.get(ch)) {
-        // TODO: encode to Algol.
-        return (uint8_t) ch;
+again:
+    if (!stream.get(ch)) {
+        // End of file.
+        return 0;
     }
-    return 0;
+    if (ch >= '0' && ch <= '9') {
+        // Digits.
+        return ch - '0';
+    }
+    if (ch >= 'a' && ch <= 'z') {
+        // Letters lowercase.
+        return ch - 'a' + 10;
+    }
+    if (ch >= 'A' && ch <= 'Z') {
+        // Letters uppercase.
+        return ch - 'A' + 37;
+    }
+    switch (ch) {
+    case '+': return 64;
+    case '-': return 65;
+    case '*': return 66; // ×
+    case '/': return 67;
+    case '>': return 70;
+    case '=': return 72;
+    case '<': return 74;
+    case ',': return 87;
+    case '.': return 88;
+    case '@': return 89; // ⏨
+    case ':': return 90;
+    case ';': return 91;
+    case ' ': return 93; // space
+    case '(': return 98;
+    case ')': return 99;
+    case '[': return 100;
+    case ']': return 101;
+    case '`': return 102;
+    case '\'': return 103;
+    case '\t': return 118; // tab
+    case '\n': return 119; // newline
+    case '\r': goto again; // CR - ignore
+    case '"': return 121;
+    case '?': return 122;
+    case '|': return 162;
+    case '_': return 163;
+    case '\302':
+        if (!stream.get(ch)) {
+            return 0;
+        }
+        switch (ch) {
+        case '\254': return 76; // ¬
+        default: goto bad;
+        }
+    case '\303':
+        if (!stream.get(ch)) {
+            return 0;
+        }
+        switch (ch) {
+        case '\227': return 66; // ×
+        case '\267': return 68; // ÷
+        default: goto bad;
+        }
+    case '\342':
+        if (!stream.get(ch)) {
+            return 0;
+        }
+        switch (ch) {
+        case '\206':
+            if (!stream.get(ch)) {
+                return 0;
+            }
+            switch (ch) {
+            case '\221': return 69; // ↑
+            default: goto bad;
+            }
+        case '\210':
+            if (!stream.get(ch)) {
+                return 0;
+            }
+            switch (ch) {
+            case '\247': return 77; // ∧
+            case '\250': return 78; // ∨
+            default: goto bad;
+            }
+        case '\211':
+            if (!stream.get(ch)) {
+                return 0;
+            }
+            switch (ch) {
+            case '\240': return 75; // ≠
+            case '\241': return 80; // ≡
+            case '\244': return 73; // ≤
+            case '\245': return 71; // ≥
+            default: goto bad;
+            }
+        case '\212':
+            if (!stream.get(ch)) {
+                return 0;
+            }
+            switch (ch) {
+            case '\203': return 79; // ⊃
+            default: goto bad;
+            }
+        case '\217':
+            if (!stream.get(ch)) {
+                return 0;
+            }
+            switch (ch) {
+            case '\250': return 89; // ⏨
+            default: goto bad;
+            }
+        default: goto bad;
+        }
+    default:
+bad:    throw std::runtime_error("Bad input character 0" + to_octal(ch & 0377));
+    }
 }
